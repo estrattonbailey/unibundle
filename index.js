3d33b052d04f21a9d51071c1171c5120502e0614
estrattonbailey <ericstrattonbailey@gmail.com>
2018-05-14 16:20:36 -0400

    Clean build dir on startup

--- index.js
+++ index.js
@@ -23,124 +23,127 @@ let loader = null
 let clientComplete = false
 let serverComplete = false
 
-if (args[0]) {
-  console.log(chalk.green(`unibundle`), chalk.gray('production'))
+/**
+ * Empty build directory first so we don't have conflicting files
+ */
+fs.emptyDir(path.resolve(process.cwd(), userConfig.buildDir), () => {
+  if (args[0]) {
+    console.log(chalk.green(`unibundle`), chalk.gray('production'))
 
-  loader = loading.start()
+    loader = loading.start()
 
-  fs.emptyDir(path.resolve(process.cwd(), userConfig.buildDir), () => {
     clientCompiler.run((err, stats) => done('client', err, stats, true))
     serverCompiler.run((err, stats) => done('server', err, stats, true))
-  })
-} else {
-  console.log(chalk.green(`unibundle`), chalk.gray('development'))
+  } else {
+    console.log(chalk.green(`unibundle`), chalk.gray('development'))
 
-  clientCompiler.hooks.done.tap({ name: 'unibundle stats' }, stats => {
-    done('client', null, stats)
-  })
-
-  userConfig.css && fs.remove(path.resolve(userConfig.publicDir, userConfig.css.output.filename))
-  fs.remove(path.resolve(userConfig.publicDir, userConfig.client.output.filename))
-
-  const server = new devServer(clientCompiler, {
-    // hot: true, // breaks, never gets update
-    publicPath: '/',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    noInfo: true,
-    clientLogLevel: 'none',
-    quiet: true
-  })
-
-  server.listen(8080, 'localhost', e => {
-    console.log(
-      chalk.green(`unibundle`),
-      chalk.gray('client bundle served at'),
-      `localhost:8080/${userConfig.client.output.filename}`
-    )
-  })
-
-  ;['SIGINT', 'SIGTERM'].forEach(function(sig) {
-    process.on(sig, function() {
-      server.close()
-      process.exit()
+    clientCompiler.hooks.done.tap({ name: 'unibundle stats' }, stats => {
+      done('client', null, stats)
     })
-  })
 
-  serverCompiler.watch(
-    { quiet: true },
-    (err, stats) => done('server', err, stats)
-  )
-}
+    userConfig.css && fs.remove(path.resolve(userConfig.publicDir, userConfig.css.output.filename))
+    fs.remove(path.resolve(userConfig.publicDir, userConfig.client.output.filename))
 
-function done (which, err, stats, production) {
-  if (err) {
-    console.error(err.stack || err);
-    if (err.details) {
-      console.error(err.details);
+    const server = new devServer(clientCompiler, {
+      // hot: true, // breaks, never gets update
+      publicPath: '/',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      noInfo: true,
+      clientLogLevel: 'none',
+      quiet: true
+    })
+
+    server.listen(8080, 'localhost', e => {
+      console.log(
+        chalk.green(`unibundle`),
+        chalk.gray('client bundle served at'),
+        `localhost:8080/${userConfig.client.output.filename}`
+      )
+    })
+
+    ;['SIGINT', 'SIGTERM'].forEach(function(sig) {
+      process.on(sig, function() {
+        server.close()
+        process.exit()
+      })
+    })
+
+    serverCompiler.watch(
+      { quiet: true },
+      (err, stats) => done('server', err, stats)
+    )
+  }
+
+  function done (which, err, stats, production) {
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      return;
     }
-    return;
-  }
 
-  const msgs = stats.toJson({}, true)
+    const msgs = stats.toJson({}, true)
 
-  if (msgs.errors.length) {
-    console.log(chalk.red('\t', 'errors'))
-    console.log()
-
-    msgs.errors.map(e => {
-      console.log('\t', e.message || e)
+    if (msgs.errors.length) {
+      console.log(chalk.red('\t', 'errors'))
       console.log()
-    })
-  }
 
-  if (msgs.warnings.length) {
-    console.log(chalk.yellow('\t', 'warnings'))
-    console.log()
+      msgs.errors.map(e => {
+        console.log('\t', e.message || e)
+        console.log()
+      })
+    }
 
-    msgs.warnings.map(e => {
-      console.log('\t', e.message || e)
+    if (msgs.warnings.length) {
+      console.log(chalk.yellow('\t', 'warnings'))
       console.log()
-    })
-  }
 
-  loading.stop(loader)
+      msgs.warnings.map(e => {
+        console.log('\t', e.message || e)
+        console.log()
+      })
+    }
 
-  if (msgs.errors.length || msgs.warnings.length) return
+    loading.stop(loader)
 
-  let size = (msgs.assets[0].size / 1024).toFixed(2) + ' kb'
+    if (msgs.errors.length || msgs.warnings.length) return
 
-  if (production && which === 'client') {
-    const file = fs.readFileSync(
-      path.resolve(userConfig.publicDir, userConfig.client.output.filename)
-    ).toString('utf8')
-    size = (gzip.sync(file) / 1024).toFixed(2) + ' kb gzipped'
-  } else if (production) {
-    size += ' minified'
-  }
+    let size = (msgs.assets[0].size / 1024).toFixed(2) + ' kb'
 
-  console.log(
-    chalk.green(`unibundle`),
-    chalk.gray(which),
-    size
-  )
+    if (production && which === 'client') {
+      const file = fs.readFileSync(
+        path.resolve(userConfig.publicDir, userConfig.client.output.filename)
+      ).toString('utf8')
+      size = (gzip.sync(file) / 1024).toFixed(2) + ' kb gzipped'
+    } else if (production) {
+      size += ' minified'
+    }
 
-  if (which === 'client') {
-    clientComplete = true
-  } else if (which === 'server') {
-    serverComplete = true
-  }
-
-  if (production && clientComplete && serverComplete) copy()
-}
-
-function copy () {
-  fs.copy(userConfig.publicDir, path.join(userConfig.buildDir, userConfig.publicDir), () => {
     console.log(
       chalk.green(`unibundle`),
-      chalk.gray('copy'),
-      `copied compiled files to /${userConfig.buildDir}`
+      chalk.gray(which),
+      size
     )
-  })
-}
+
+    if (which === 'client') {
+      clientComplete = true
+    } else if (which === 'server') {
+      serverComplete = true
+    }
+
+    if (production && clientComplete && serverComplete) copy()
+  }
+
+  function copy () {
+    fs.copy(userConfig.publicDir, path.join(userConfig.buildDir, userConfig.publicDir), () => {
+      console.log(
+        chalk.green(`unibundle`),
+        chalk.gray('copy'),
+        `copied compiled files to /${userConfig.buildDir}`
+      )
+    })
+  }
+})
